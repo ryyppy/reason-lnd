e340cfdb28450541217317a5411dd504784eb3c5
Patrick Stapfer <ryyppy@users.noreply.github.com>
2017-09-05 21:57:39 +0200

    Fix errors caused by missing Convert module

--- backend/src/routes.ml
+++ backend/src/routes.ml
@@ -1,14 +1,16 @@
 open Cohttp
 open Cohttp_lwt_unix
-open State
 
 (* OH! what is this? Mutable global state? Gee, we all gonna die~~ *)
-let currentState: (state option) ref = ref None;;
+let currentState: (State.state option) ref = ref None;;
+
+let serializeState state =
+  Yojson.to_string (State.json_of_state state)
 
 let saveRoute =
   Server.respond_string ~status:`OK ~body:"/save route" ()
 
 let loadRoute =
   match !currentState with
-  | Some s -> Server.respond_string ~status:`OK ~body:(Convert.stateToString s) ()
+  | Some s -> Server.respond_string ~status:`OK ~body:(serializeState s) ()
   | None -> Server.respond_error ~status:`Bad_request ~body:"Bad request" ()
