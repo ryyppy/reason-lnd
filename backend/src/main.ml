c2c60a69e7fbeca6a1909cd141718b87b18f91c8
Patrick Stapfer <ryyppy@users.noreply.github.com>
2017-09-05 12:51:30 +0200

    Make the server recognize /save and /load routes

--- backend/src/main.ml
+++ backend/src/main.ml
@@ -1,4 +1,37 @@
-let run () =
-  print_endline "Hello Server";;
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
 
-let _ = run ()
+let getPort =
+  try
+    int_of_string (Sys.getenv "PORT")
+  with
+  | Failure _ | Not_found -> 8000
+
+let saveRoute =
+  Server.respond_string ~status:`OK ~body:"/save route" ()
+
+let loadRoute =
+  Server.respond_string ~status:`OK ~body:"/load route" ()
+
+let handleGet req =
+  let path = req |> Request.uri |> Uri.path |> String.split_on_char '/' |> List.tl in
+  match path with
+  | [""] -> Server.respond_not_found ()
+  | [name] -> (match name with
+    | "save" -> saveRoute
+    | "load" -> loadRoute
+    | _ -> Server.respond_not_found ())
+  | _ -> Server.respond_not_found ()
+
+let server port =
+  let callback _conn req body =
+    let meth = (req |> Request.meth) in
+    match meth with
+    | `GET -> req |> handleGet
+    | _ -> Server.respond_error ~status:`Method_not_allowed ~body:(Code.string_of_method meth) ()
+   in
+   Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
+
+let () = ignore (Lwt_main.run (server getPort))
+
